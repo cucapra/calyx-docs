5d4d24ebe80ad43110767630f9bfaacf31bda668
rachitnigam <rachitnigam@users.noreply.github.com>
2023-05-10 04:25:27 +0000

    deploy: cucapra/calyx@053816482479ef359f88b1fc223ba99278533f76

---
+++ source/implementors/strum/trait.EnumCount.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"calyx_frontend":[["impl EnumCount for <a class=\"enum\" href=\"calyx_frontend/enum.BoolAttr.html\" title=\"enum calyx_frontend::BoolAttr\">BoolAttr</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
