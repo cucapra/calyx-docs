ff1d4c681e2093189a8207ccec93ce776bda5cb1
Basant-khalil <Basant-khalil@users.noreply.github.com>
2023-08-10 15:12:11 +0000

    deploy: cucapra/calyx@95206fa900e1483c2461c7cca25eeb26a6219897

--- source/implementors/calyx_utils/position/trait.WithPos.js
+++ source/implementors/calyx_utils/position/trait.WithPos.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"calyx_frontend":[["impl <a class=\"trait\" href=\"calyx_utils/position/trait.WithPos.html\" title=\"trait calyx_utils::position::WithPos\">WithPos</a> for <a class=\"struct\" href=\"calyx_frontend/struct.Attributes.html\" title=\"struct calyx_frontend::Attributes\">Attributes</a>"]],
-"calyx_ir":[["impl <a class=\"trait\" href=\"calyx_utils/position/trait.WithPos.html\" title=\"trait calyx_utils::position::WithPos\">WithPos</a> for <a class=\"enum\" href=\"calyx_ir/enum.Control.html\" title=\"enum calyx_ir::Control\">Control</a>"]]
+"calyx_frontend":[["impl WithPos for <a class=\"struct\" href=\"calyx_frontend/struct.Attributes.html\" title=\"struct calyx_frontend::Attributes\">Attributes</a>"]],
+"calyx_ir":[["impl WithPos for <a class=\"enum\" href=\"calyx_ir/enum.Control.html\" title=\"enum calyx_ir::Control\">Control</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
