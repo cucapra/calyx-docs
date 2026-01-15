a927be8953737cc7da2a66ba70281b4257221c38
jku20 <jku20@users.noreply.github.com>
2026-01-15 14:46:29 +0000

    deploy: calyxir/calyx@5f6f86ab4b553bb32328f5c497791af00fd70229

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7bb61c73.js',
+                path_to_root + 'searchindex-94412a93.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
