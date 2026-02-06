fb4553c81afc77b0f32e2cd4dce8aaba070f2125
EclecticGriffin <EclecticGriffin@users.noreply.github.com>
2026-02-06 18:06:24 +0000

    deploy: calyxir/calyx@97d9ae749a7b5b0b34f7facd104c589513a9c4cc

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-94412a93.js',
+                path_to_root + 'searchindex-cd3920bb.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
