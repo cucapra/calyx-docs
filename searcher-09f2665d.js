a4b81a817f0d3f61f78d39e5f59273a99b16e782
ayakayorihiro <ayakayorihiro@users.noreply.github.com>
2026-06-10 14:22:16 +0000

    deploy: calyxir/calyx@43424bde22b0ddeafae8b88b5618296b086000e2

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-70534f9a.js',
+                path_to_root + 'searchindex-bb3bc527.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
